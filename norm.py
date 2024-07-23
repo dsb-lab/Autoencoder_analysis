247419a94269c1617d300b125a8d2aef0c41f30e
omarablanco <omarblanco400@gmail.com>
2024-07-23 16:06:52 +0200

    Update norm.py

--- norm.py
+++ norm.py
@@ -8,30 +8,6 @@ def cut_norm(data,time):
     data=data.loc[data['Time [s]']<time]
     return data
 
-def reinterpolate_and_sample (data,nvalues_to_interpolate):
-    aux=data['Time [s]'].max()/(nvalues_to_interpolate+1)
-    aux=int(aux)
-    for i in range(nvalues_to_interpolate):
-        trigger_value=aux*(i+1)
-        index = (data['Time [s]'] > trigger_value).idxmax()
-        aux2=data['Time [s]'][index]+(((data['Time [s]'][index+1]-data['Time [s]'][index]))/2)
-        x=data['Time [s]'].values
-        new_array = np.empty(data.shape[1],dtype=float)
-        new_array[0]=aux2
-        i=1
-        for column in data.columns.values[2:]:
-            y=data[column].values
-            aux3=np.interp(aux2,x,y) 
-            new_array[i]=aux3
-            i+=1
-        data.loc[index-0.5] = new_array    #introduciendo la nueva fila
-        data = data.sort_index().reset_index(drop=True)
-
-    #data=data.sample(frac=0.4,axis=0)
-    data=data.loc[(data['Cycle Nr.']==1) | (data['Cycle Nr.']%3==0)]
-    return data
-
-
 def interpolate_and_sample(data,time_to_cut,nvalues_to_interpolate):
     aux=data['Time [s]'].max()
     x_to_interpolate=np.linspace(0,time_to_cut,num=nvalues_to_interpolate)
@@ -52,7 +28,6 @@ def interpolate_and_sample(data,time_to_cut,nvalues_to_interpolate):
     
         new_data[column]= new_array    #Introducing the new interpolated column
 
-    #data=data.sample(frac=0.4,axis=0)
     new_data=new_data.loc[(new_data['Cycle Nr.']==1) | (new_data['Cycle Nr.']%3==0)]    #Sampling of 1/3
 
     return new_data
